7af6cfc3a54bea0a991db515936f16d772f1cc38
CatrionaLAnderson <catrionalanderson@outlook.com>
2025-05-28 13:30:40 +0100

    updates
    Buy a zine page, new supabase

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js';
 
 
-const supabaseUrl = 'https://btmbpbllhmhgegzshwzi.supabase.co'; // Replace with your Supabase URL
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0bWJwYmxsaG1oZ2VnenNod3ppIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMDkyODg0NiwiZXhwIjoyMDQ2NTA0ODQ2fQ.hH06StOWCi7r33m4XbET7XaDEJoiXPDFeV7prt83TaQ';
+const supabaseUrl = 'https://autunitllvjblvjsionn.supabase.co'; // Replace with your Supabase URL
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF1dHVuaXRsbHZqYmx2anNpb25uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0MzM4ODcsImV4cCI6MjA2NDAwOTg4N30.ToOyrlhFctE2QxIwpj79gQz7-GBaVXmIBl2jsU6d6JA';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
