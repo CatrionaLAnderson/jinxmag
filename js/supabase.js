fa82e3e710a004f8d3dcc7cbdff81bfba390090b
CatrionaLAnderson <catrionalanderson@outlook.com>
2025-05-29 15:13:12 +0100

    fix??

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js';
 
 const supabaseUrl = 'https://autunitllvjblvjsionn.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF1dHVuaXRsbHZqYmx2anNpb25uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0MzM4ODcsImV4cCI6MjA2NDAwOTg4N30.ToOyrlhFctE2QxIwpj79gQz7-GBaVXmIBl2jsU6d6JA'
