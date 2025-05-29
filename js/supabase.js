83917721f964aceb9c9e1c214e63c088eb1aabb3
CatrionaLAnderson <catrionalanderson@outlook.com>
2025-05-29 15:07:44 +0100

    fix????

--- js/supabase.js
+++ js/supabase.js
@@ -1,9 +1,8 @@
-import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-
-const supabaseUrl = 'https://autunitllvjblvjsionn.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF1dHVuaXRsbHZqYmx2anNpb25uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0MzM4ODcsImV4cCI6MjA2NDAwOTg4N30.ToOyrlhFctE2QxIwpj79gQz7-GBaVXmIBl2jsU6d6JA';
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = 'https://autunitllvjblvjsionn.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF1dHVuaXRsbHZqYmx2anNpb25uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0MzM4ODcsImV4cCI6MjA2NDAwOTg4N30.ToOyrlhFctE2QxIwpj79gQz7-GBaVXmIBl2jsU6d6JA'
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
 
